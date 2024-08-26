1f5b8159782480bc95bb9c0645aed17f6a33b2e2
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-08-26 18:27:06 +0530

    Imported bo Impl classes for BOFactory

--- src/main/java/lk/kavishka/restorisejakarthaeebackend/bo/BOFactory.java
+++ src/main/java/lk/kavishka/restorisejakarthaeebackend/bo/BOFactory.java
@@ -1,5 +1,11 @@
 package lk.kavishka.restorisejakarthaeebackend.bo;
 
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.impl.CustomerBOImpl;
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.impl.ItemBOImpl;
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.impl.OrderBOImpl;
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.impl.OrderDetailBOImpl;
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.impl.StaffBOImpl;
+
 public class BOFactory {
-    
+
 }
