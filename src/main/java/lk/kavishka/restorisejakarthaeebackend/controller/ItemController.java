91483e9dabce648e491e8ce9180d5972ef792202
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-08-28 23:14:26 +0530

    Update ItemController

--- src/main/java/lk/kavishka/restorisejakarthaeebackend/controller/ItemController.java
+++ src/main/java/lk/kavishka/restorisejakarthaeebackend/controller/ItemController.java
@@ -1,4 +1,101 @@
 package lk.kavishka.restorisejakarthaeebackend.controller;
 
-public class ItemController {
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.kavishka.restorisejakarthaeebackend.bo.BOFactory;
+import lk.kavishka.restorisejakarthaeebackend.bo.custom.ItemBO;
+import lk.kavishka.restorisejakarthaeebackend.dto.ItemDTO;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemController extends HttpServlet {
+    private Connection connection;
+    ItemBO itemBO = (ItemBO) BOFactory.getBOFactory().BOTypes(BOFactory.BOTypes.ITEMS);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(var writer = resp.getWriter()){
+            List<ItemDTO> itemDTOList = itemBO.getAllItem(connection);
+            if (itemDTOList != null){
+                resp.setContentType("application/json");
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(itemDTOList,resp.getWriter());
+            }else {
+                writer.write("Not Available");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!req.getContentType().toLowerCase().contains("application/json") || req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        Jsonb jsonb = JsonbBuilder.create();
+        ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+
+        try(var writer = resp.getWriter()){
+            boolean isSaved = itemBO.saveItem(itemDTO,connection);
+            if (isSaved){
+                writer.write("successfully");
+            }else {
+                writer.write("Try Again");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ItemDTO itemDTO = JsonbBuilder.create().fromJson(req.getReader(), ItemDTO.class);
+        try(var writer = resp.getWriter()){
+            boolean isUpdated = itemBO.updateItem(itemDTO,connection);
+            if (isUpdated){
+                writer.write("update successfully");
+            }else {
+                writer.write("Please Try Again.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ItemDTO itemDTO = JsonbBuilder.create().fromJson(req.getReader(), ItemDTO.class);
+        try(var writer = resp.getWriter()){
+            var isDelete = itemBO.deleteItem(itemDTO.getItemId(), connection);
+            if (isDelete){
+                writer.write("Delete Successfully");
+            }else {
+                writer.write("Please Try again!");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        try{
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/restoRise");
+            this.connection = pool.getConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
